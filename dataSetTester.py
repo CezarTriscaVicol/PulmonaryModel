c6eca052c2292860f6fd0ced349fca8965103fac
CezarTriscaVicol <padua_robotica@aleeas.com>
2022-12-04 16:07:32 +0000

    Same as last commit

--- dataSetTester.py
+++ dataSetTester.py
@@ -1,14 +1,25 @@
 import numpy as np
+import main
+import dataSetClass
+import matplotlib.pyplot as plt
 
-fileName  = "lung16_continuous.npy"
-dataset = np.memmap(fileName, dtype = 'float64', mode='r')
-dataset.resize((int(dataset.shape[0]/23), 23))
-print(dataset.shape)
-print(dataset[0])
-print(dataset[-1])
-print(np.max(dataset[:,0]))
-print(np.min(dataset[:,0]))
-print(np.mean(dataset[:,0]))
-print(np.max(dataset[:,-1]))
-print(np.min(dataset[:,-1]))
-print(np.mean(dataset[:,-1]))
+for case in range(4):
+    
+    fileName = 'datasets/lung'+str(main.nodeCount)+'_continuous'+str(case)+'.npy'
+    dataset = np.memmap(fileName, dtype = 'float64', mode='r')
+    dataset.resize((int(dataset.shape[0]/dataSetClass.columns), dataSetClass.columns))
+    
+    volumes = dataset[:,:dataSetClass.featureCount]
+    restrictions = dataset[:,dataSetClass.featureCount:]
+    
+    print("Dataset Shape: ", dataset.shape)
+    print(np.max(volumes), " | ", np.min(volumes), " | ", np.mean(volumes))
+    print(np.max(restrictions), " | ", np.min(restrictions), " | ", np.mean(restrictions))
+
+    plt.style.use('_mpl-gallery')
+    
+    fig, ax = plt.subplots()
+    VP = ax.boxplot(restrictions[:,np.subtract(main.oneEachDepth, 1)], positions=range(4), widths=0.7, patch_artist=True,
+                    showmeans=True, showfliers=False)
+
+    plt.show()
